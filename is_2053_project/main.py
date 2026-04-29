8d74b1c0d75a91d64c4aece15f2309bb111f0067
bouyfloat <liam.endress@my.utsa.edu>
2026-04-29 22:07:17 +0000

    commit

--- is_2053_project/main.py
+++ is_2053_project/main.py
@@ -1,6 +1,15 @@
 def main():
     while True:
         try:
-            user_status = input("Are you a new user (0) or a returning user (1)? ")
+            user_status = input("Are you a new user (n) or a returning user (r)? ")
+        except TypeError: print("Invalid input. Try again.")
+        else:
+            if user_status == 'n' or user_status == 'r':
+                break
+            else:
+                print("Invalid input. Try again.")
+    #login dictionaries
+    #menu
+    #score updates
 if __name__ == "__main__":
     main()
